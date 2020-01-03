60b3b9356607e93403b305d087d41b026ec2d98a
Sapto Aji <saosabc321@gmail.com>
2020-01-04 01:57:25 +0700

    first commit

---
+++ package/LatihanPackage/src/app/Program.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Program {
+    public static void main(String[] args) {
+        p1.Test obj1 = new p1.Test();
+        p2.Test obj2 = new p2.Test();
+        obj1.tampilTeks();
+        obj2.tampilTeks();
+    }
+}
