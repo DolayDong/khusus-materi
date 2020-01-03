f40fefb336c7a0413ca4d870756db137d2f0af64
Sapto Aji <saosabc321@gmail.com>
2020-01-04 01:50:48 +0700

    do while sederhana

---
+++ Pengulangan/BelajarDoWhile.java
@@ -0,0 +1,12 @@
+package Pengulangan;
+
+public class BelajarDoWhile {
+
+    public static void main(String[] args) {
+        int baterry = 0;
+
+        do {
+            System.out.println("Nyalakan hp");
+        } while (baterry > 0);
+    }
+}
