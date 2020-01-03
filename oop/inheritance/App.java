0c0cfbbed73f90c674ce4a2f4046f430ac4367d7
Sapto Aji <saosabc321@gmail.com>
2020-01-04 01:54:28 +0700

    first commit

---
+++ oop/inheritance/App.java
@@ -0,0 +1,50 @@
+class person {
+    // atribute
+    String jeneng;
+    int umur;
+    double tinggiBadan;
+    double beratBadan;
+
+    // method
+    void jalan() {
+        // code untuk berjalan
+        System.out.println("Saya sedang berjalan");
+    }
+
+    void bicara(String kata) {
+        System.out.println(this.jeneng + " berkata : '" + kata + "'");
+    }
+
+    void aturNama(String aran) {
+        this.jeneng = aran;
+    }
+
+    String getName() {
+        return this.jeneng;
+    }
+
+    void sebutNama() {
+        System.out.println(this.getName());
+    }
+
+}
+
+public class App {
+
+    public static void main(String[] args) {
+        // instansiasi class menjadi object
+        person sapto = new person();
+        person nanang = new person();
+
+        sapto.aturNama("Sapto Aji");
+        System.out.print("Hallo nama saya ");
+        sapto.sebutNama();
+        sapto.bicara("Hahaha saya adalah Ironman");
+
+        nanang.aturNama("Nanang Fauzi");
+        nanang.sebutNama();
+        nanang.bicara("Haha dan saya adik ironman");
+
+    }
+
+}
