e48b9004223b73b21b6db8bd97a2ea92c449d97f
Sapto Aji <saosabc321@gmail.com>
2020-01-04 01:57:07 +0700

    first commit

---
+++ package/ClassTwo.java
@@ -0,0 +1,10 @@
+package packageTwo;
+
+import packageOne.ClassOne;
+
+public class ClassTwo {
+    public static void main(String[] args) {
+        ClassOne objClassOne = new ClassOne();
+        objClassOne.tampilTeks();
+    }
+}
